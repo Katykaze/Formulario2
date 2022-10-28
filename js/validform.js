7551bd3d4b71fdfb18a116441415e29c70a85b4f
Katykaze <kathezambrano9@gmail.com>
2022-10-28 13:03:03 +0200

    con expresion regular fecha

--- js/validform.js
+++ js/validform.js
@@ -295,8 +295,7 @@ function limpiar() {
 function validarFecha() {
     let valido = true;
     let fecha = document.formulario.fecha.value;
-    let regExpfecha = /^(\d{1,2})*(\/|-)*(\d{1,2})*(\/|-)*(\d{4})$/;
-    //(\d{1,2})
+    let regExpfecha = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
     if (!regExpfecha.test(fecha)) {
 
         document.formulario.error_fecha.value = "Error, La fecha es incorrecta. ";
